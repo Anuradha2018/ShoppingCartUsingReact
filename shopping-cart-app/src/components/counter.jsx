0c9678a67c785fc5137cefcd588eebd4c3e96044
Anuradha <anumo@dtu.dk>
2019-05-08 21:48:56 +0200

    Object destructuring

--- shopping-cart-app/src/components/counter.jsx
+++ shopping-cart-app/src/components/counter.jsx
@@ -7,10 +7,17 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span>{this.state.count}</span>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </div>
     );
+  }
+  formatCount() {
+    //Object destructuring
+    // Old: return this.state.count === 0 ? "Zero" : this.state.count;
+    //New: after destructuring
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
