2d5130e740a89843f9ad404d01ff24e175b80008
Anuradha <anumo@dtu.dk>
2019-05-08 20:51:58 +0200

    bug fix

--- shopping-cart-app/src/index.js
+++ shopping-cart-app/src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from "react-dom";
 const element = <h1>Hello World</h1>;
 console.log(element);
 
+ReactDOM.render(element, document.getElementById("root"));
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
