2cd22b2a4d4f27005fd0a1fcfa23b044de5de9ca
Anuradha <anumo@dtu.dk>
2019-05-09 21:11:47 +0200

    Conditional rendering

--- shopping-cart-app/src/components/counter.jsx
+++ shopping-cart-app/src/components/counter.jsx
@@ -5,32 +5,23 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
-  render() {
-    let classes = this.getBadgeClasses();
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
     return (
-      <div>
-        <span className={classes}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    //Object destructuring
-    // Old: return this.state.count === 0 ? "Zero" : this.state.count;
-    //New: after destructuring
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && "Please create a new tag"}
+        {this.renderTags()}
+      </div>
+    );
   }
 }
 
