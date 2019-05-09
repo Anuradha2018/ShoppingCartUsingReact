3a0078bb4369bd7ee53ab0dcf1e087146296af86
Anuradha <anumo@dtu.dk>
2019-05-09 22:30:18 +0200

    Better way
    Whenever we need to pass an argument to the event handler simply pass the arrow function

--- shopping-cart-app/src/components/counter.jsx
+++ shopping-cart-app/src/components/counter.jsx
@@ -22,16 +22,14 @@ class Counter extends Component {
     console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
-  doHandleIncrement = () => {
-    this.handleIncrement({ id: 1 });
-  };
+
   render() {
     let classes = this.getBadgeClasses();
     return (
       <div>
         <span className={classes}>{this.formatCount()}</span>
         <button
-          onClick={this.doHandleIncrement}
+          onClick={() => this.handleIncrement({ product })}
           className="btn btn-secondary btn-sm"
         >
           Increment
