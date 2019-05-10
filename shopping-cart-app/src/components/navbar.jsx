252e81965da30ad5e10065394d519c448dc4b987
Anuradha <anumo@dtu.dk>
2019-05-10 22:44:41 +0200

    Stateless Functional components
    When is stateless component required?
    When we have one single render method, we dont have any event handlers, helper-methods to calculate values. We only have props to get data and there is no state

--- shopping-cart-app/src/components/navbar.jsx
+++ shopping-cart-app/src/components/navbar.jsx
@@ -1,17 +1,18 @@
-import React, { Component } from "react";
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Shopping Cart
-          <span className="badge badge-pill badge-seconday">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+import React from "react";
+// stateless functional component
+// instead of using a component we can use a function to do it.
+
+const Navbar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Shopping Cart
+        <span className="badge badge-pill badge-seconday">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
