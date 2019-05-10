94d7e7c5012fd2d03b84ce16a5b4ed3a0f5ef0dd
Anuradha <anumo@dtu.dk>
2019-05-10 22:10:32 +0200

    Multiple components in sync
    When there is no parent child relationship between two components , and we want to keep them in sync and we want to share data between them, we need  to lift the state up.

--- shopping-cart-app/src/App.js
+++ shopping-cart-app/src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Navbar from "./components/navbar";
 import "./App.css";
+import Counters from "./components/counters";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="Äpp-title">Welcome to react</h1>
-        </header>
-        <p className="Äpp-intro">
-          To get started, edit <code>src/App.js</code> and save to reload
-        </p>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <main className="container">
+          <Counters />
+        </main>
+      </React.Fragment>
     );
   }
 }
